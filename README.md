ad5d28a8d21042efadd0a298e3952713a716ee7f
toproad <tattun0817@gmail.com>
2023-05-04 09:54:45 +0900

    ⚡️ コードの整形や、ロールの追加

--- README.md
+++ README.md
@@ -1,33 +1,62 @@
 # chatgpt-slack-gcp
-slack apiでchatGPTと壁打ちするためのソースコードです。
-gcpとなっているが、当初使用する予定だったgcpではなく、AWS Lambdaでの実装方法を説明します。
-## 実装手順
-1️ Slack Appを作成し、SLACK_SIGNING_SECRETとSLACK_BOT_TOKENを取得したものを環境変数に設定する。
-2 OPENAI_API_KEYを取得し、環境変数に設定する。
-3 npm install
-　npm run build
-　cd dist
-　zip -r deploy_package.zip ./bundle.js ../node_modules
-　を順に実行する。
-　最後のコマンドにより、distディレクトリにdeploy_package.zipが作成される。
-4 AWS Lambdaにdeploy_package.zipをアップロードする。
-5 AWS Lambdaの設定で、環境変数を設定するともに、エンドポイントを作成する。
-6 AWS Lambdaのランタイム設定を[bundle.handler]に変更する。
-　※デフォルトでは[index.handler]になっているが、作成したファイルがbundle.jsなので、変更する。
-7 AWS Lambdaの設定で、API Gatewayの設定によってエンドポイントを作成する。
-  エンドポイントの設定で、APIのタイプをREST APIにし、エンドポイントタイプをリージョンに設定する。
-8 Slack Appの設定で、Event Subscriptionsを有効にし、Request URLにAWS Lambdaのエンドポイントを設定する。
-9 Slack Appの設定で、Bot Userを有効にする。
-  Subscribe to bot eventsにapp_mentionを追加する。
-  
 
+slack api で chatGPT と壁打ちするためのソースコードです。
+gcp となっているが、当初使用する予定だった gcp ではなく、AWS Lambda での実装方法を説明します。
+
+## 実装手順
+
+1️ Slack App を作成し、SLACK_SIGNING_SECRET と SLACK_BOT_TOKEN を取得したものを環境変数に設定する。
+2 OPENAI_API_KEY を取得し、環境変数に設定する。
+3 npm install
+　 npm run build
+　 cd dist
+　 zip -r deploy_package.zip ./bundle.js ../node_modules
+　を順に実行する。
+　最後のコマンドにより、dist ディレクトリに deploy_package.zip が作成される。
+4 AWS Lambda に deploy_package.zip をアップロードする。
+5 AWS Lambda の設定で、環境変数を設定するともに、エンドポイントを作成する。
+6 AWS Lambda のランタイム設定を[bundle.handler]に変更する。
+　※デフォルトでは[index.handler]になっているが、作成したファイルが bundle.js なので、変更する。
+7 AWS Lambda の設定で、API Gateway の設定によってエンドポイントを作成する。
+エンドポイントの設定で、API のタイプを REST API にし、エンドポイントタイプをリージョンに設定する。
+8 Slack App の設定で、Event Subscriptions を有効にし、Request URL に AWS Lambda のエンドポイントを設定する。
+9 Slack App の設定で、Bot User を有効にする。
+Subscribe to bot events に app_mention を追加する。
+
+## 今後の使い方
+- ロールについて
+      const chatCompletionRequestMessage: ChatCompletionRequestMessage[] = [
+        {
+          role: 'system',
+          content: 'You are a full-stack engineer.',
+        },
+        {
+          role: 'system',
+          content: 'You are experienced in JavaScript, Python, and SQL.',
+        },
+        {
+          role: 'system',
+          content:
+            'You have experience working with React, Django, and PostgreSQL.',
+        },
+      ];
+
+の部分のロールを書き換えます。
+- 精度について
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: chatCompletionRequestMessage,
+        // temperature: 0.8, // ここで temperature を設定
+        // top_p: 1.0, // ここで top_p を設定
+      });
+の  temperatureとtop_pを変えます。
 ## 実装の参考にしたサイト
-- Node.jsの実装やslack apiの設定
-https://qiita.com/Yuki_Oshima/items/112e69df63df9958709f
+
+- Node.js の実装や slack api の設定
+  https://qiita.com/Yuki_Oshima/items/112e69df63df9958709f
 
 - SlackBolt
-https://slack.dev/bolt-js/ja-jp/deployments/aws-lambda#set-up-aws-lambda
+  https://slack.dev/bolt-js/ja-jp/deployments/aws-lambda#set-up-aws-lambda
 
-- AWS lambdaの実装
-https://qiita.com/melty_go/items/a6929b0a341e75d24f01
-
+- AWS lambda の実装
+  https://qiita.com/melty_go/items/a6929b0a341e75d24f01
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import {App, AwsLambdaReceiver} from '@slack/bolt';
+import { App, AwsLambdaReceiver } from "@slack/bolt";
 import {
   AwsCallback,
   AwsEvent,
-} from '@slack/bolt/dist/receivers/AwsLambdaReceiver';
-import {isAxiosError} from 'axios';
-import {ChatCompletionRequestMessage, Configuration, OpenAIApi} from 'openai';
+} from "@slack/bolt/dist/receivers/AwsLambdaReceiver";
+import { isAxiosError } from "axios";
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
 
 if (!process.env.SLACK_SIGNING_SECRET) process.exit(1);
 
@@ -21,52 +21,66 @@ const app = new App({
   receiver: awsLambdaReceiver,
 });
 
-const bot_userid = '...';
+const bot_userid = "...";
 
-app.event('app_mention', async ({event, context, client, say}) => {
+app.event("app_mention", async ({ event, context, client, say }) => {
   if (context.retryNum) {
     console.log(`skipped retry. retryReason: ${context.retryReason}`);
     return;
   }
   console.log(event);
   try {
-    const {channel, thread_ts, event_ts} = event;
+    const { channel, thread_ts, event_ts } = event;
     const threadTs = thread_ts ?? event_ts;
     await say({
       channel,
       thread_ts: threadTs,
-      text: '`system` 処理中……',
+      text: "`system` 回答作成中",
     });
     try {
       const threadResponse = await client.conversations.replies({
         channel,
         ts: threadTs,
       });
-      const chatCompletionRequestMessage: ChatCompletionRequestMessage[] = [];
-      threadResponse.messages?.forEach(message => {
-        const {text, user} = message;
+      // あらかじめフルスタックエンジニアの役割を設定。配列の中に複数配置しても良い
+      const chatCompletionRequestMessage: ChatCompletionRequestMessage[] = [
+        {
+          role: "system",
+          content: "You are a full-stack engineer.",
+        },
+        {
+          role: "system",
+          content: "You are experienced in JavaScript, Python, and SQL.",
+        },
+        {
+          role: "system",
+          content:
+            "You have experience working with React, Django, and PostgreSQL.",
+        },
+      ];
+      threadResponse.messages?.forEach((message) => {
+        const { text, user } = message;
         if (!text) return;
+        const content = text.replace(`<@${bot_userid}>`, "") ?? "";
         if (user && user === bot_userid) {
-          if (!text.startsWith('`system`')) {
-            chatCompletionRequestMessage.push({
-              role: 'assistant',
-              content: text,
-            });
+          if (text.startsWith("`system`")) {
+            chatCompletionRequestMessage.push({ role: "system", content });
+          } else {
+            chatCompletionRequestMessage.push({ role: "assistant", content });
           }
         } else {
-          chatCompletionRequestMessage.push({
-            role: 'user',
-            content: text.replace(`<@${bot_userid}>`, '') ?? '',
-          });
+          chatCompletionRequestMessage.push({ role: "user", content });
         }
       });
       const completion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
+        model: "gpt-3.5-turbo",
         messages: chatCompletionRequestMessage,
+        // temperature: 0.8, // ここで temperature を設定
+        // top_p: 1.0, // ここで top_p を設定
       });
       const outputText = completion.data.choices
-        .map(({message}) => message?.content)
-        .join('');
+        .map(({ message }) => message?.content)
+        .join("");
       await client.chat.postMessage({
         channel,
         thread_ts: threadTs,
@@ -81,7 +95,7 @@ app.event('app_mention', async ({event, context, client, say}) => {
       await client.chat.postMessage({
         channel,
         thread_ts: threadTs,
-        text: '`system` エラーが発生しました。(管理人: <@...>)',
+        text: "`system` エラーが発生しました。(管理人: <@...>)",
       });
     }
   } catch (error) {
@@ -89,11 +103,15 @@ app.event('app_mention', async ({event, context, client, say}) => {
   }
 });
 
-module.exports.handler = async (event: AwsEvent, context: unknown, callback: AwsCallback) => {
-  if (event.httpMethod === 'POST' && event.body) {
+module.exports.handler = async (
+  event: AwsEvent,
+  context: unknown,
+  callback: AwsCallback,
+) => {
+  if (event.httpMethod === "POST" && event.body) {
     const body = JSON.parse(event.body);
 
-    if (body.type === 'url_verification') {
+    if (body.type === "url_verification") {
       return {
         statusCode: 200,
         body: body.challenge,
